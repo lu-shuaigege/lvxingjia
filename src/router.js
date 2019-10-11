466be91efeb4f9735dcd17e93828e17710882440
lu-shuaigege <lu17372621727@163.com>
2019-10-11 18:16:57 +0800

    2019-10-11-02

--- src/router.js
+++ src/router.js
@@ -1,25 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import home from './views/home/home.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
+export const contentsRouter = [{
+    path: '/',
+    redirect: 'home',
+},
+{
+    path: '/home',
+    name: 'home',
+    meta: {
+        title: '主页'
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+    component: () =>
+        import('./views/home/home.vue')
+},
+]
+
+export default new Router({
+    mode: 'hash',
+    base: process.env.BASE_URL,
+    routes: contentsRouter
 })
---
+++ src/views/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+import tab from '@/components/tab/tab.vue'
+
+export default {
+  name: 'home',
+  components: {
+    tab
+  }
+}
+</script>
