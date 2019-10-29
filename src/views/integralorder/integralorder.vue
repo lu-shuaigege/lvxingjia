30f4218b974c28d1dfdc2a9313cd0b5389795002
lu-shuaigege <lu17372621727@163.com>
2019-10-29 18:30:26 +0800

    2019-10-29-05

--- src/views/integralorder/integralorder.vue
+++ src/views/integralorder/integralorder.vue
@@ -3,8 +3,56 @@
     <div class="integralorder-top">
       <div class="top-con">
         <div class="top-con-left">您的收货地址为空，点此添加收货地址</div>
-        <img src="../../assets/img/Linelist/4-1.png" class="top-con-right" alt />
+        <img src="../../assets/img/Linelist/right.png" class="top-con-right" alt />
       </div>
+    </div>
+    <div class="integralorder-topnew">
+      <img src="../../assets/img/Integralmall/addressicon.png" class="top-con-left" alt />
+      <div class="top-con-center">
+        <div class="top-con-center-top">
+          <div class="top-con-center-top-name">{{address.name}}</div>
+          <div class="top-con-center-top-tel">{{address.tel}}</div>
+        </div>
+        <div class="top-con-center-bottom">{{address.content}}</div>
+      </div>
+      <img src="../../assets/img/Linelist/right.png" class="topnew-con-right" alt />
+    </div>
+    <div class="integralorder-center">
+      <div class="center-left">
+        <img :src="order.url" class="center-left-img" alt />
+      </div>
+      <div class="center-right">
+        <div class="center-right-top">{{order.title}}</div>
+        <div class="center-right-center">{{order.content}}</div>
+        <div class="center-right-bottom">
+          <div class="center-right-bottom-left">{{order.integral}}积分</div>
+          <div class="center-right-bottom-right">x1</div>
+        </div>
+      </div>
+    </div>
+    <div class="integralorder-center2">
+      <div class="center2-top">
+        <div class="center2-top-left">配送方式</div>
+        <div class="center2-top-right">顺丰到付</div>
+      </div>
+      <div class="center2-center">
+        <div class="center2-center-left">备注(选填)</div>
+        <input type="text" class="center2-center-right" placeholder="请填写备注内容" />
+      </div>
+      <div class="center2-bottom">
+        <div class="center2-bottom-left">共1件商品</div>&nbsp;&nbsp;
+        <div class="center2-bottom-right">
+          小计 :
+          <div>{{order.integral}}积分</div>
+        </div>
+      </div>
+    </div>
+    <div class="payorder">
+      <div class="payorder-left">
+        <div class="payorder-left-left">合计 :</div>&nbsp;&nbsp;
+        <div class="payorder-left-right">¥ 2980.00</div>
+      </div>
+      <div class="payorder-right">提交订单</div>
     </div>
   </div>
 </template>
@@ -14,7 +62,19 @@ export default {
   name: "integralorder",
 
   data() {
-    return {};
+    return {
+      order: {
+        url: require("../../assets/img/Linelist/4-1.png"),
+        title: "登山杖碳素超轻超短伸",
+        content: "相关简介文案相关简介文案相关简介文案相关简介文案相关简介文案",
+        integral: "15500"
+      },
+      address: {
+        name: "林凯丽",
+        tel: "15962336196",
+        content: "江苏省苏州市苏州工业园区苏州工业园区直属纳米科技E栋1602"
+      }
+    };
   },
   components: {},
   watch: {},
